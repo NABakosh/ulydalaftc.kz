91ef0eeccd15f346dedf77edf0b64d0f33ac54d6
KINGOFMUSIC <GoldonGame.06@gmail.com>
2025-02-02 22:00:18 +0500

    React Router

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
-const Header = () =>{
-    return(
-			<header>
-				<nav>
-					<img width={75} height={97} src='img/home/logo.png' alt="Logo"/>
-					<a>Home</a>
-					<a>About Us</a>
-					<a>Robot</a>
-					<a>Events</a>
-				</nav>
-			</header>
-)}
-export default Header;
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+	return (
+		<header>
+			<nav>
+				<img width={75} height={97} src='img/home/logo.png' alt='Logo' />
+				<Link to='/'>Home</Link>
+				<Link to='/AboutUs'>About Us</Link>
+			</nav>
+		</header>
+	)
+}
+
+export default Header
