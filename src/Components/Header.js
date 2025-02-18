c4b7a26259b87e61c066c935adb8dd07f27f165a
Mike <asansabatyrov@gmail.com>
2025-02-18 14:00:00 +0300

    Описание изменений

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,15 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
-		<header>
-			<nav>
-				<Link to='/'>Home</Link>
-				<Link to='/events'>Events</Link>
-				<img width={75} height={97} src='/img/home/logo.png' alt='Logo' />
-				<Link to='/robot'>Robot</Link>
-				<Link to='/team'>Team</Link>
-
+		<header className='header'>
+			<nav className='nav'>
+				<ul className='list'>
+					<Link to='/'>Home</Link>
+					<Link to='/events'>Events</Link>
+					<img src='/img/home/logo.png' alt='Logo' />
+					<Link to='/robot'>Robot</Link>
+					<Link to='/team'>Team</Link>
+				</ul>
 			</nav>
 		</header>
 	)
