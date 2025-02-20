ee794db56700d3e0bc0da506bc3ce0a1042971ff
NABako <goldongame.06@gmail.com>
2025-02-20 13:56:44 +0500

    123

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import './index.scss'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './Components/Header'
-import Home from './Home'
-import Footer from './Components/Footer'
-import OurRobot from './OurRobot'
-import OurTeam from './OurTeam'
-import Event from './Event'
-import EventDetails from './Components/EventDetails'
-import LoadingScreen from './Components/LoadingScreen'
 import { useEffect, useState } from 'react'
 
 function App() {
 	const [loading, setLoading] = useState(true)
 
 	useEffect(() => {
-		// Закрытие экрана после полной загрузки контента с задержкой 2 секунды
-		window.onload = () => {
-		  setTimeout(() => {
-			setLoading(false);
-		  }, 1200); // Задержка в 2000 миллисекунд (2 секунды)
-		};
-	  }, []);
-	  
+		// Проверяем, загрузился ли уже документ
+		if (document.readyState === 'complete') {
+			setTimeout(() => setLoading(false), 1200)
+		} else {
+			// Добавляем слушатель загрузки страницы
+			const handleLoad = () => {
+				setTimeout(() => setLoading(false), 1200)
+			}
+			window.addEventListener('load', handleLoad)
+
+			// Убираем слушатель при размонтировании
+			return () => window.removeEventListener('load', handleLoad)
+		}
+	}, [])
 
 	return (
 		<>
-			<div
-				className={`loading-container ${
-					loading ? 'show-loading' : 'hide-loading'
-				}`}
-			>
-				<LoadingScreen />
-			</div>
+			{loading && (
+				<div className='loading-container'>
+					<LoadingScreen />
+				</div>
+			)}
 			<div className={`App ${loading ? 'content-hidden' : 'content-visible'}`}>
 				<Router>
 					<Header />
@@ -48,5 +42,3 @@ function App() {
 		</>
 	)
 }
-
-export default App
